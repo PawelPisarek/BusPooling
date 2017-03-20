2ecb4eb620533a45a92ef0cb5d2fcd5637c8f28b
Paweł Pisarek <pisarekp73@gmail.com>
2017-03-20 14:19:58 +0100

    cors

--- src/main/java/BusPooling/configurations/CorsConfiguration.java
+++ src/main/java/BusPooling/configurations/CorsConfiguration.java
@@ -13,6 +13,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 public class CorsConfiguration extends WebMvcConfigurerAdapter {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("http://localhost:3000").allowedHeaders("*").allowedMethods("*");
+        registry.addMapping("/**").allowedOrigins("http://localhost:4200").allowedHeaders("*").allowedMethods("*");
     }
 }
